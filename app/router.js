af518bca3abf33e51d39b6d4d7e86a0188eef701
richo-x <richo@mmxii.me>
2016-03-18 01:30:01 -0600

    UPDATE - router

--- app/router.js
+++ app/router.js
@@ -20,16 +20,16 @@ Router.map(function() {
     this.route('edit', { path: '/edit/:calendario_id' });
   });
 
-  this.resource('tasks', function() {
+  this.resource('tareas', function() {
     this.route('new');
-    this.route('show', { path: '/show/:task_id' });
-    this.route('edit', { path: '/edit/:task_id' });
+    this.route('show', { path: '/show/:tarea_id' });
+    this.route('edit', { path: '/edit/:tarea_id' });
   });
 
-  this.resource('notes', function() {
+  this.resource('notas', function() {
     this.route('new');
-    this.route('show', { path: '/show/:note_id' });
-    this.route('edit', { path: '/edit/:note_id' });
+    this.route('show', { path: '/show/:nota_id' });
+    this.route('edit', { path: '/edit/:nota_id' });
   });
 
 });
