686428748ac999abcbb9a5d3ee6761f2f2bd726b
pinky <orochi_alex@hotmail.com>
2016-03-18 11:18:40 -0600

    post contact

--- app/router.js
+++ app/router.js
@@ -6,6 +6,32 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+
+  this.resource('contactos', function() {
+    this.route('new',  { path: '/new' });
+    this.route('show', { path: '/show/:contacto_id' });
+    this.route('edit', { path: '/edit/:contacto_id' });
+  });
+
+  this.resource('calendars', function() {
+    this.route('new');
+    this.route('show', { path: '/show/:calendar_id' });
+    this.route('edit', { path: '/edit/:calendar_id' });
+  });
+
+  this.resource('tasks', function() {
+    this.route('new');
+    this.route('show', { path: '/show/:task_id' });
+    this.route('edit', { path: '/edit/:task_id' });
+  });
+
+  this.resource('notes', function() {
+    this.route('new');
+    this.route('show', { path: '/show/:note_id' });
+    this.route('edit', { path: '/edit/:note_id' });
+  });
+
 });
 
 export default Router;
--- app/routes/contactos/index.js
+++ app/routes/contactos/index.js
@@ -1,19 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    content: [],
+
 	model: function() {
-		//return this.store.find('contacto', {'accessToken': 'ya29.pALPk-PK5PXv0JLlJzAi62gr-bRo9RzQv42_z69zCY9mabVxFI8Xkg44ySB_J-cgHx0'}).then(function(data) {
-			//var response = Ember.Object.create(data);
-			//console.log('response --> ' ,response.content );
-			//var response = serializer.extract();
-			//response.forEach(function(item) {
-			//	console.log('data ---> ' + item )
-			//});
-		//});
-		var test = Ember.ArrayProxy.create({ content: [ {contacto: "Juan", test: "pruebas", email: "test@co.com" }]});
-		
+
+        var controller = this;
+        var accessToken = 'ya29.qQKeP5AObnNm-xXVOKx8wjCLARp9VP7LBD7kibqLp7vL6cSzXGD1VRAY5ZQsSaznipw';
+  		Ember.$.ajax({
+            url:'http://tudu.m3x1c0.com:3000/api/users/contactos?accessToken=' + accessToken,
+			type: 'GET',
+			dataType: 'json',
+			contentType: 'application/json; charset=iso-8859-1;',
+		//	data: JSON.stringify(payload)
+		})
+		.done(function(data) {
+			var entry = data.contactoList.feed.entry;
+			for (var i = 0; i < entry.length; i++) {
+				var element = Ember.ArrayProxy.create( {contacto: entry[i]["title"]["$t"] , test: "pruebas", email: entry[i]["gd$email"][0]["address"] });
+				controller.get('content').pushObject(element);
+			};
+
+		})
+		.fail(function() {
+			alert('fail');
+		});
+
+		var test = Ember.ArrayProxy.create({ content: controller.get('content')});
+
 		return test;
-		
+
 	},
 	setupController: function(controller, model) {
 		this._super(controller, model);
